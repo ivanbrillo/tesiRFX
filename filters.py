908c9ca24d1fa78908d3d89823dbc12361c3133b
ivanbrillo <117514607+ivanbrillo@users.noreply.github.com>
2023-11-05 21:02:49 +0100

    Update filters.py

--- filters.py
+++ filters.py
@@ -1,46 +1,65 @@
-import tesiRFX.plot_drawer as plot_drawer   # custom module
 import pandas as pd
 from scipy import signal
 import numpy as np
 
 
-def get_mov_avg(df: pd.DataFrame, win_size: int) -> pd.DataFrame:
-  column_names = plot_drawer.get_columns(df)  # get all the column with the exclusion of TIME
-  smoothed_df = pd.DataFrame(df["TIME"])
+def get_mov_avg(data_list: list, win_size: int) -> list:
+    smoothed_list = list()
+    for data_element in data_list:
+        smoothed_list.append({
+            "date": data_element["date"],
+            "type": data_element["type"],
+            "frequency": data_element["frequency"],
+            "exposure time": data_element["exposure time"],
+            "supply delay": data_element["supply delay"],
+            "time_data": data_element["time_data"].rolling(window=win_size).mean()
+        })
 
-  for name in column_names:
-    smoothed_df[name] = df[name].rolling(window=win_size).mean()
-
-  return smoothed_df
-
-
-def get_simple_exp(df: pd.DataFrame, alpha: int) -> pd.DataFrame:
-  column_names = plot_drawer.get_columns(df)
-  smoothed_df = pd.DataFrame(df["TIME"])
-
-  for name in column_names:
-    smoothed_df[name] = df[name].ewm(alpha=alpha).mean()
-  
-  return smoothed_df
+    return smoothed_list
 
 
-def get_savitzy_golay(df: pd.DataFrame, win_len: int, pol_order) -> pd.DataFrame:
-  column_names = plot_drawer.get_columns(df)
-  smoothed_df = pd.DataFrame(df["TIME"])
+def get_simple_exp(data_list: list, alpha: int) -> list:
+    smoothed_list = list()
+    for data_element in data_list:
+        smoothed_list.append({
+            "date": data_element["date"],
+            "type": data_element["type"],
+            "frequency": data_element["frequency"],
+            "exposure time": data_element["exposure time"],
+            "supply delay": data_element["supply delay"],
+            "time_data": data_element["time_data"].ewm(alpha=alpha).mean()
+        })
 
-  for name in column_names:
-    smoothed_df[name] = signal.savgol_filter(df[name], window_length=win_len, polyorder=pol_order, mode="nearest")
-
-  return smoothed_df
+    return smoothed_list
 
 
-def get_FFT(df: pd.DataFrame, n_freq: int) -> pd.DataFrame:
-  column_names = plot_drawer.get_columns(df)
-  smoothed_df = pd.DataFrame(df["TIME"])
+def get_savitzy_golay(data_list: list, win_len: int, pol_order) -> list:
+    smoothed_list = list()
+    for data_element in data_list:
+        smoothed_list.append({
+            "date": data_element["date"],
+            "type": data_element["type"],
+            "frequency": data_element["frequency"],
+            "exposure time": data_element["exposure time"],
+            "supply delay": data_element["supply delay"],
+            "time_data": pd.Series(signal.savgol_filter(data_element["time_data"], window_length=win_len, polyorder=pol_order, mode="nearest"))
+        })
 
-  for name in column_names:
-    rft = np.fft.rfft(df[name].to_numpy())   # performs a real-valued Fast Fourier Transform (FFT), return array of frequency
-    rft[n_freq:] = 0                         # removes high-frequency components from the FFT. Note, rft.shape = 451
-    smoothed_df[name] = np.fft.irfft(rft)    # performs the inverse FFT to get back in the time domain
+    return smoothed_list
 
-  return smoothed_df
+
+def get_FFT(data_list: list, n_freq: int) -> list:
+    smoothed_list = list()
+    for data_element in data_list:
+        rft = np.fft.rfft(data_element["time_data"].to_numpy())  # performs a real-valued Fast Fourier Transform (FFT), return array of frequency
+        rft[n_freq:] = 0  # removes high-frequency components from the FFT. Note, rft.shape = 451
+        smoothed_list.append({
+            "date": data_element["date"],
+            "type": data_element["type"],
+            "frequency": data_element["frequency"],
+            "exposure time": data_element["exposure time"],
+            "supply delay": data_element["supply delay"],
+            "time_data": pd.Series(np.fft.irfft(rft))  # performs the inverse FFT to get back in the time domain  
+        })
+
+    return smoothed_list
