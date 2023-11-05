8959e3ab39deb2781ddc8f646826eaa73b2aec9a
ivanbrillo <117514607+ivanbrillo@users.noreply.github.com>
2023-11-05 20:30:59 +0100

    Update plot_drawer.py

--- plot_drawer.py
+++ plot_drawer.py
@@ -1,26 +1,21 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 
-def get_columns(dataframe: pd.DataFrame) -> list:
-    column_names = dataframe.columns.values.tolist()
-    del column_names[0]  # remove TIME column
-    return column_names
-
-
-def subplot(dataframe: pd.DataFrame, axes: list, legend: str = None):
-    for index, name in enumerate(get_columns(dataframe)):
-        legend_value = name if legend is None else f"{legend} {name}"
-        axes[index].plot(dataframe["TIME"], dataframe[name], label=legend_value)
+def subplot(data_list: list, axes: list, legend: str = None):
+    for index, series in enumerate(data_list):
+        legend_str = f"{series['date']} {series['type']} {series['frequency']} {series['exposure time']}' {series['supply delay']}"
+        legend_value = legend_str if legend is None else f"{legend_str} {legend}"
+        
+        axes[index].plot(series["time_data"].index.tolist(), series["time_data"].values, label=legend_value)
         axes[index].legend(loc='lower right')
 
 
-def plot(original_data: pd.DataFrame, modified_data: list = None, modified_legend: str = None) -> None:
+def plot(original_data: list, modified_data: list = None, modified_legend: str = None) -> None:
     #  1xN grid of subplot
-    fig, axes = plt.subplots(1, len(get_columns(original_data)), figsize=(15, 3))
+    fig, axes = plt.subplots(1, len(original_data), figsize=(5 * len(original_data), 5))
     subplot(original_data, axes)
 
     if modified_data is not None:
-      for index, frame in enumerate(modified_data):
-        subplot(frame, axes, f'({index}) {modified_legend}' )
+        subplot(modified_data, axes, modified_legend)
 
     plt.show()
