f6822ac70dd0bda50c102a5fea468a4cbd8202e9
ivanbrillo2 <117514607+ivanbrillo@users.noreply.github.com>
2023-11-29 09:38:00 +0100

    data visualizer

--- tesiRFX/plot_drawer.py
+++ tesiRFX/plot_drawer.py
@@ -1,5 +1,5 @@
 import matplotlib.pyplot as plt
-import pandas as pd
+import math
 
 
 def subplot(data_list: list, axes: list, legend: str = None):
@@ -7,8 +7,9 @@ def subplot(data_list: list, axes: list, legend: str = None):
         legend_str = f"{series['date']} {series['type']} {series['frequency']} {series['exposure time']} {series['supply delay']}"
         legend_value = legend_str if legend is None else f"{legend_str} {legend}"
 
-        axes[index].plot(series["time_data"].index.tolist(), series["time_data"].values, label=legend_value)
-        axes[index].legend(loc='lower right', fontsize=7)
+        axes[int(index / 4)][index % 4].plot(series["time_data"].index.tolist(), series["time_data"].values,
+                                             label=legend_value)
+        axes[int(index / 4)][index % 4].legend(loc='lower right', fontsize=7)
 
 
 def plot(original_data: list, modified_data: list = None, modified_legend: str = None) -> None:
@@ -19,4 +20,15 @@ def plot(original_data: list, modified_data: list = None, modified_legend: str =
     if modified_data is not None:
         subplot(modified_data, axes, modified_legend)
 
+    plt.show()
+
+
+def grid_plot(original_data: list) -> None:
+    #  (N/4)x4 grid of subplot
+    N = len(original_data)
+    fig, axes = plt.subplots(ncols=4, nrows=math.ceil(N / 4), layout='constrained',
+                             figsize=(3.5 * 4, 3.5 * math.ceil(N / 4)))
+
+    subplot(original_data, axes)
+    # plt.tight_layout()
     plt.show()
