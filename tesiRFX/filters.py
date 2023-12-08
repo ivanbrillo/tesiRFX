2427fea59b9f31b3405acf56348717f33a70b634
ivanbrillo2 <117514607+ivanbrillo@users.noreply.github.com>
2023-12-08 08:13:47 +0100

    refactoring + atmf corrected

--- tesiRFX/filters.py
+++ tesiRFX/filters.py
@@ -3,99 +3,65 @@ from scipy import signal
 import numpy as np
 
 
-def get_mov_avg(data_list: list, win_size: int) -> list:
-    smoothed_list = list()
-    for data_element in data_list:
-        smoothed_list.append({
-            "date": data_element["date"],
-            "type": data_element["type"],
-            "frequency": data_element["frequency"],
-            "exposure time": data_element["exposure time"],
-            "supply delay": data_element["supply delay"],
-            "time_data": data_element["time_data"].rolling(window=win_size).mean()
+def _create_dict(original_list: list, smoothed_data: list) -> list:
+    data_list = list()
+    for data_dict, smoothed_list in zip(original_list, smoothed_data):
+        data_list.append({
+            "date": data_dict["date"],
+            "type": data_dict["type"],
+            "frequency": data_dict["frequency"],
+            "exposure time": data_dict["exposure time"],
+            "supply delay": data_dict["supply delay"],
+            "time_data": smoothed_list
         })
+    return data_list
 
-    return smoothed_list
+
+def get_mov_avg(data_list: list, win_size: int) -> list:
+    smoothed_data = [data["time_data"].rolling(window=win_size).mean() for data in data_list]
+    return _create_dict(data_list, smoothed_data)
 
 
 def get_simple_exp(data_list: list, alpha: int) -> list:
-    smoothed_list = list()
-    for data_element in data_list:
-        smoothed_list.append({
-            "date": data_element["date"],
-            "type": data_element["type"],
-            "frequency": data_element["frequency"],
-            "exposure time": data_element["exposure time"],
-            "supply delay": data_element["supply delay"],
-            "time_data": data_element["time_data"].ewm(alpha=alpha).mean()
-        })
-
-    return smoothed_list
+    smoothed_data = [data["time_data"].ewm(alpha=alpha).mean() for data in data_list]
+    return _create_dict(data_list, smoothed_data)
 
 
 def get_savitzy_golay(data_list: list, win_len: int, pol_order) -> list:
-    smoothed_list = list()
-    for data_element in data_list:
-        smoothed_list.append({
-            "date": data_element["date"],
-            "type": data_element["type"],
-            "frequency": data_element["frequency"],
-            "exposure time": data_element["exposure time"],
-            "supply delay": data_element["supply delay"],
-            "time_data": pd.Series(
-                signal.savgol_filter(data_element["time_data"], window_length=win_len, polyorder=pol_order,
-                                     mode="nearest"))
-        })
-
-    return smoothed_list
+    smoothed_data = [pd.Series(
+        signal.savgol_filter(data["time_data"], window_length=win_len, polyorder=pol_order,
+                             mode="nearest")) for data in data_list]
+    return _create_dict(data_list, smoothed_data)
 
 
 def get_FFT(data_list: list, n_freq: int) -> list:
-    smoothed_list = list()
+    rft_list = list()
     for data_element in data_list:
-        rft = np.fft.rfft(data_element[
-                              "time_data"].to_numpy())  # performs a real-valued Fast Fourier Transform (FFT), return array of frequency
-        rft[n_freq:] = 0  # removes high-frequency components from the FFT. Note, rft.shape = 451
-        smoothed_list.append({
-            "date": data_element["date"],
-            "type": data_element["type"],
-            "frequency": data_element["frequency"],
-            "exposure time": data_element["exposure time"],
-            "supply delay": data_element["supply delay"],
-            "time_data": pd.Series(np.fft.irfft(rft))  # performs the inverse FFT to get back in the time domain  
-        })
+        # performs a real-valued Fast Fourier Transform (FFT), return array of frequency
+        rft_list.append(np.fft.rfft(data_element["time_data"].to_numpy()))
+        rft_list[-1][n_freq:] = 0  # removes high-frequency components from the FFT. Note, rft.shape = 451
 
-    return smoothed_list
+    # performs the inverse FFT to get back in the time domain
+    smoothed_data = [pd.Series(np.fft.irfft(rft)) for rft in rft_list]
+    return _create_dict(data_list, smoothed_data)
 
 
-def alpha_trimmed_mean_filter(data_list: list, win_size: int, alpha: int):
+def _atmf(data: list, win_size: int, alpha: int) -> pd.Series:
+    N = len(data)
+    result = [0] * (N - win_size)
 
-    smoothed_list = list()
+    assert win_size > alpha > 0 and alpha % 4 == 0 and win_size % 2 == 0
 
-    for data_element in data_list:
+    for i in range(win_size // 2, N - win_size // 2):
+        window = data[i - win_size // 2:i + win_size // 2]
+        window.sort()
 
-        N = len(data_element["time_data"])
-        data = data_element["time_data"].tolist()
+        # Get the result - the mean value of the elements in the trimmed set
+        result[i - win_size // 2] = sum(window[alpha // 4:win_size - (alpha // 4) * 3]) / (win_size - alpha)
 
-        # Initialize the result list
-        result = [0] * (N - win_size)
+    return pd.Series([result[0]] * (win_size // 2) + result + [result[-1]] * (win_size // 2))
 
-        # Move window through all elements of the signal
-        for i in range(win_size // 2, N - win_size // 2):
-            # Pick up window elements
-            window = data[i - win_size // 2:i + win_size // 2]
-            window.sort()
 
-            # Get the result - the mean value of the elements in the trimmed set
-            result[i - win_size // 2] = sum(window[alpha // 2:win_size - (alpha // 2)]) / (win_size - alpha)
-
-        smoothed_list.append({
-            "date": data_element["date"],
-            "type": data_element["type"],
-            "frequency": data_element["frequency"],
-            "exposure time": data_element["exposure time"],
-            "supply delay": data_element["supply delay"],
-            "time_data": pd.Series(result)
-        })
-
-    return smoothed_list
+def alpha_trimmed_mean_filter(data_list: list, win_size: int, alpha: int) -> list:
+    smoothed_data = [_atmf(data["time_data"].tolist(), win_size, alpha) for data in data_list]
+    return _create_dict(data_list, smoothed_data)
