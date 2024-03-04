6bc2021413b524fd1271eae3043262b448a8fc4e
ivanbrillo <117514607+ivanbrillo@users.noreply.github.com>
2024-03-04 09:19:15 +0100

    Update autoencoder_helper.py

--- tesiRFX/autoencoder_helper.py
+++ tesiRFX/autoencoder_helper.py
@@ -37,7 +37,6 @@ class CustomMSE(losses.Loss):
 
 
 class PlotLearning(Callback):
-
     def on_train_begin(self, logs={}):
         self.metrics = {}
         for metric in logs:
@@ -53,20 +52,27 @@ class PlotLearning(Callback):
 
         metrics = [x for x in logs if 'val' not in x]
         f, axs = plt.subplots(1, len(metrics), figsize=(15, 5))
-        plt.ylim(0, 0.05)
+
+        if not isinstance(axs, np.ndarray) :
+          axs = (axs, )
+          plt.ylim(0, 0.05)
+        else:
+          for ax in axs:
+            ax.set_ylim((0, 10))
+
         clear_output(wait=True)
 
         for i, metric in enumerate(metrics):
-            axs.plot(range(1, epoch + 2),
+            axs[i].plot(range(1, epoch + 2),
                      self.metrics[metric],
                      label=metric)
             if logs['val_' + metric]:
-                axs.plot(range(1, epoch + 2),
+                axs[i].plot(range(1, epoch + 2),
                          self.metrics['val_' + metric],
                          label='val_' + metric)
 
-            axs.legend()
-            axs.grid()
+            axs[i].legend()
+            axs[i].grid()
 
         plt.tight_layout()
         plt.show()
